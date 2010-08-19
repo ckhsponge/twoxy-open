b6039b0ce0abdcc639505f9a420a974c1fc9fd1e
ckhsponge <ckhobbs@gmail.com>
2010-08-19 16:31:16 -0700

    httpparty

--- app/controllers/fetch_controller.rb
+++ app/controllers/fetch_controller.rb
@@ -1,6 +1,9 @@
 
 require 'net/http'
 require 'uri'
+#require 'http_encoding_helper' #for gzip support use response.plain_body
+require 'httparty'
+
 class FetchController < ApplicationController
   EXPIRE_FETCH = 1.minutes
   EXPIRE_CONTENT = 15.minutes
@@ -9,17 +12,35 @@ class FetchController < ApplicationController
     @sources = params[:sources].split("\n").collect{|s| s.strip} if params[:sources]
   end
   
+  #uses Net::HTTP
   def url
+    serve_cached_data_for_source do |source|
+        response = fetch_url(source)
+        #puts response.body
+        response.body
+    end
+  end
+  
+  #uses httparty lib
+  def httparty
+    serve_cached_data_for_source("HTTPPARTY") do |source|
+      response = HTTParty.get(source)
+      #puts response.body
+      response.body
+    end
+  end
+  
+  protected
+  #cache_prepend distinguishes caches
+  def serve_cached_data_for_source( cache_prepend = "" )
     source = params[:source]
     unless source
       render :text => "no url specified"
       return
     end
-    result = data_cache.fetch(source, :expires_in => EXPIRE_FETCH ) do
+    result = data_cache.fetch("#{cache_prepend}#{source}", :expires_in => EXPIRE_FETCH ) do
       begin
-        response = fetch_url(source)
-        #puts response.body
-        response.body
+        yield(source)
       rescue Exception => exc
         logger.error exc.to_s
         "not found, please wait #{EXPIRE_FETCH} seconds and try again (#{exc.to_s})"
@@ -30,7 +51,6 @@ class FetchController < ApplicationController
     render :text => result
   end
   
-  protected
   def fetch_url(uri_str, limit = 10)
     puts "fetch_url #{uri_str}"
     # You should choose better exception.
@@ -39,7 +59,15 @@ class FetchController < ApplicationController
     url = URI.parse(uri_str)
     request = Net::HTTP::Get.new(url.path)
     #fake the user agent
-    #request['User-Agent'] = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3"
+#    request['User-Agent'] = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.3) Gecko/20100401 Firefox/3.6.3"
+#    request['Accept'] = "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+#    request['Accept-Charset'] = "ISO-8859-1,utf-8;q=0.7,*;q=0.7"
+#    request['Host'] = "www.google.com"
+#    request['Accept-Language'] = "en-us,en;q=0.5"
+#    request['Accept-Encoding'] = "gzip,deflate"
+#    request['Keep-Alive'] = "115"
+#    request['Connection'] = "keep-alive"
+#    puts request.to_hash.inspect
     response = Net::HTTP.start(url.host, url.port) {|http|
       http.request(request)
     }
